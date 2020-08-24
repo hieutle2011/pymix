55344141dacc6c62aba1e7790db65a809fb03ed7
Hieu Le <hieuletrung102@gmail.com>
2020-08-24 16:49:37 +0700

    ex 4.2

--- lib/ex4.ex
+++ lib/ex4.ex
@@ -17,3 +17,22 @@ defmodule Ex41 do
     |> String.pad_leading(@num_length, @zero)
   end
 end
+
+defmodule Ex42 do
+  @target 0o777
+
+  @doc """
+    Trả về giá trị cần cộng thêm với octal để thu được 0o777
+
+    Với người dùng Unix(Ubuntu, MacOS,...), mode của một file được biểu diễn ở
+    dạng Octal, VD: 644, 400, 777...
+
+    Gợi ý:
+
+    In [1]: oct(73)
+    Out[1]: '0o111'
+  """
+  def solve(octal) do
+    @target - octal
+  end
+end
